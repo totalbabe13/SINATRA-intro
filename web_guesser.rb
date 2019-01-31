a61665451ab99d4333214b58b2056d7e887de894
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-01-31 12:27:00 -0600

    change html ccs colors from ruby files

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,23 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
 number = rand(100)
-def check_guess(guess,number)
+def check_guess(guess,number)  
+	
 	if guess == number
-		message = "YOU GUESSED IT! The SECRET number is #{number}"	
+		message = "<p style='color:green';> YOU GUESSED IT! The SECRET number is #{number} </p>"	
 	elsif guess > number + 5
-	    message = 'WAY too high!'
+	    message = "<p style='color:red';> WAY too HIGH! </p>"
 	elsif guess > number  && guess < number + 5
-	    message = 'Too high'	    
+	    message = "<p style='color:#ffb3b3';>  Too high </p>"	    
     elsif guess < number - 5
-     	message = 'WAY too low!'
+     	message = "<p style='color:red';> WAY too LOW! </p>"
     elsif guess < number && guess > number - 5
-        message = "Too low!"  	
+        message = "<p style='color:#ffb3b3';>  Too low </p>" 	
     end	
 end    
   
 get '/' do
 	guess = params["guess"].to_i
 	message = check_guess(guess,number)
-    erb :index, :locals => {:number => number, :message => message}
+    erb :index, :locals => {:number => number, :message => message }
 end
