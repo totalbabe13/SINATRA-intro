f78bf8dbd3018cfdde77c632c7dba7366a6163c2
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-01-31 11:50:26 -0600

    sintra tutorial almost finished

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,10 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100) 
-
-get '/' do
+number = rand(100)
+def check_guess(guess,number)
+	if guess == number
+		message = "YOU GUESSED IT! The SECRET number is #{number}"	
+	elsif guess > number + 5
+	    message = 'WAY too high!'
+	elsif guess > number  && guess < number + 5
+	    message = 'Too high'	    
+    elsif guess < number - 5
+     	message = 'WAY too low!'
+    elsif guess < number && guess > number - 5
+        message = "Too low!"  	
+    end	
+end    
   
-  erb :index, :locals => {:number => number}
-
+get '/' do
+	guess = params["guess"].to_i
+	message = check_guess(guess,number)
+    erb :index, :locals => {:number => number, :message => message}
 end
